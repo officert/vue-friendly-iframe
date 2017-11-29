eb396dc10f78f639c1f091273e0cd32c988bb2da
Tim Officer <timothyofficer@gmail.com>
2017-11-29 17:54:26 -0500

    initial commit

---
+++ friendlyIframe.vue
@@ -0,0 +1,42 @@
+<template>
+<div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'friendly-iframe',
+  props: {
+    scriptSrc: {
+      type: String,
+      required: true
+    },
+    className: {
+      type: String,
+      required: false
+    }
+  },
+  data() {
+    return {
+      iframeEl: null
+    };
+  },
+  computed: {},
+  methods: {},
+  mounted() {
+    this.iframeEl = document.createElement('iframe');
+    this.iframeEl.setAttribute('crossorigin', 'anonymous');
+    this.iframeEl.setAttribute('scrolling', 'no');
+    this.iframeEl.setAttribute('target', '_parent');
+    if (this.className) this.iframeEl.setAttribute('class', this.className);
+    this.$el.replaceWith(this.iframeEl);
+
+    const iframeDoc = this.iframeEl.contentWindow.document;
+    iframeDoc.open().write(`<body onload="window.location.href='${this.scriptSrc}'"></body>`);
+
+    iframeDoc.close(); //iframe onload event happens
+
+    this.$emit('load');
+  }
+};
+</script>
